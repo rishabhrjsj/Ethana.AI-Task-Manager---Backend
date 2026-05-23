29403fc1cdddb05e44cf8ad038b103918c568511
Rishabh Jaiswal <rishabhrjjaiwal@gmail.com>
2026-05-23 12:56:50 +0530

    cors issue

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: config.frontendUrl,
+    origin: [
+      'http://localhost:5173',
+      'https://ethanaai-task-manager-frontend-production.up.railway.app',
+    ],
     credentials: true,
   })
 );
